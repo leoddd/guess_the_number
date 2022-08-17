602bad657141461b02431b17857b8329f13e19a3
Tim Schellenberg <tim@agentur-pottkinder.de>
2022-08-17 08:52:34 +0200

    Add templating, make flask available through port 80

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from database import Database
+
 app = Flask(__name__)
+db = Database()
 
 @app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+def initialize():
+    arguments = request.args;
+    try:
+        game = db.get_game(arguments['game'])
+    except:
+        return render_template('newGame.html')
+
+    return render_template(
+        'main.html',
+        args = request.args
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
