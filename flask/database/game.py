9187928bfc5b873db4c48c2290d753dc0756a812
Tim Schellenberg <tim@agentur-pottkinder.de>
2022-08-17 11:27:44 +0200

    Make Models typed

--- flask/database/game.py
+++ flask/database/game.py
@@ -1,5 +1,5 @@
 class Game:
-    def __init__(self, id, name, correctGuess, guesses):
+    def __init__(self, id: int = None, name: str = "", correctGuess: int = 10, guesses: list = []):
         self.id = id
         self.name = name
         self.correctGuess = correctGuess
@@ -8,9 +8,9 @@ class Game:
     def __str__(self):
         g = ""
         for guess in self.guesses:
-            g = g + "\n\t" + guess.toString()
+            g = g + "\n\t" + str(guess)
 
         if len(g) == 0:
             g = "\n\tNone"
 
-        return "Game -> id: " + str(self.id) + ", name: " + str(self.name) + ", correctGuess: " + str(self.correctGuess) + ", guesses: " + g
+        return "[Game -> id: " + str(self.id) + ", name: " + str(self.name) + ", correctGuess: " + str(self.correctGuess) + ", guesses: " + g + "]"
--- flask/database/guess.py
+++ flask/database/guess.py
@@ -1,9 +1,11 @@
+import datetime
+
 class Guess:
-    def __init__(self, id, gameId, date, guess):
+    def __init__(self, id: int = None, gameId: int = None, date: datetime = None, guesses: list = []):
         self.id = id
         self.gameId = gameId
         self.date = date
-        self.guess = guess
+        self.guesses = guesses
 
     def __str__(self):
-        return "Guess -> id: " + str(self.id) + ", gameId: " + str(self.gameId) + ", date: " + self.date.strftime("%m/%d/%Y, %H:%M:%S") + ", guess: " + str(self.guess)
+        return "[Guess -> id: " + str(self.id) + ", gameId: " + str(self.gameId) + ", date: " + self.date.strftime("%m/%d/%Y, %H:%M:%S") + ", guesses: " + str(self.guesses) + "]"
